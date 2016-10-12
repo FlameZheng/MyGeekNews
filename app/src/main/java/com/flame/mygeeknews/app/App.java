ae196911b039b6911e28e70f2ea287500ea0e939
FlameZheng <zhengtianji001@163.com>
2016-10-12 16:15:51 +0800

    # 创建项目结构

---
+++ app/src/main/java/com/flame/mygeeknews/app/App.java
@@ -0,0 +1,16 @@
+package com.flame.mygeeknews.app;
+
+import android.app.Application;
+
+/**
+ * 作者：flamezheng（郑天吉）
+ * 时间： on 2016/10/12 0012 16:04
+ * 邮箱：zhengtianji001@163.com
+ * 描述：程序的入口
+ */
+public class App extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
---
+++ app/src/main/java/com/flame/mygeeknews/app/constant.java
@@ -0,0 +1,11 @@
+package com.flame.mygeeknews.app;
+
+/**
+ * 作者：flamezheng（郑天吉）
+ * 时间： on 2016/10/12 0012 16:04
+ * 邮箱：zhengtianji001@163.com
+ * 描述：保存用到的常量
+ */
+public class Constant {
+
+}
