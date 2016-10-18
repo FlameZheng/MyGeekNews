c713a3560126fed3331fb38dfea257f5ddebff79
FlameZheng <zhengtianji001@163.com>
2016-10-18 18:02:47 +0800

    # 搭建基本框架

--- app/src/main/java/com/flame/mygeeknews/ui/main/activity/MainActivity.java
+++ app/src/main/java/com/flame/mygeeknews/ui/main/activity/MainActivity.java
@@ -1,7 +1,9 @@
-package com.flame.mygeeknews;
+package com.flame.mygeeknews.ui.main.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.flame.mygeeknews.R;
 
 public class MainActivity extends AppCompatActivity {
 
